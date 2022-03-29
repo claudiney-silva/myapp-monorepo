c7b5e532ba56baafb9242c5378b64bbed1f6d5a8
Claudiney Calixto da Silva <effetivo@effetivo.com.br>
2022-03-29 15:34:31 -0300

    Added bazByProxy

--- apps/bar/src/main/java/com/github/claudineysilva/monorepo/apps/bar/BarApplication.java
+++ apps/bar/src/main/java/com/github/claudineysilva/monorepo/apps/bar/BarApplication.java
@@ -1,6 +1,7 @@
 package com.github.claudineysilva.monorepo.apps.bar;
 
 import com.github.claudineysilva.monorepo.libs.contracts.bar.BarDto;
+import com.github.claudineysilva.monorepo.libs.contracts.baz.BazDto;
 import com.github.claudineysilva.monorepo.libs.contracts.foo.FooDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -24,10 +25,15 @@ public class BarApplication {
 		return BarDto.builder().id(UUID.randomUUID().toString()).barAttribute("barValue").build();
 	}
 
+	@GetMapping("bazByProxy")
+	BazDto baz() {
+		var restTemplate = new RestTemplate(); // Yes, should be a bean.
+		return restTemplate.getForObject("http://localhost:8081/baz", BazDto.class);
+	}
+
 	@GetMapping("fooByProxy")
 	FooDto foo() {
 		var restTemplate = new RestTemplate(); // Yes, should be a bean.
 		return restTemplate.getForObject("http://localhost:8082/foo", FooDto.class);
 	}
-
 }
