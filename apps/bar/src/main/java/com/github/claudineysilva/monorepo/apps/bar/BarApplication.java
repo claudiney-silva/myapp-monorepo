c46781985ee709179a9dd28b0b1fb8c50baed12f
Claudiney Calixto da Silva <effetivo@effetivo.com.br>
2022-03-29 15:39:32 -0300

    Log and zulu distribution

--- apps/bar/src/main/java/com/github/claudineysilva/monorepo/apps/bar/BarApplication.java
+++ apps/bar/src/main/java/com/github/claudineysilva/monorepo/apps/bar/BarApplication.java
@@ -3,6 +3,7 @@ package com.github.claudineysilva.monorepo.apps.bar;
 import com.github.claudineysilva.monorepo.libs.contracts.bar.BarDto;
 import com.github.claudineysilva.monorepo.libs.contracts.baz.BazDto;
 import com.github.claudineysilva.monorepo.libs.contracts.foo.FooDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import java.util.UUID;
 
+@Slf4j
 @RestController
 @RequestMapping
 @SpringBootApplication
@@ -27,6 +29,7 @@ public class BarApplication {
 
 	@GetMapping("bazByProxy")
 	BazDto baz() {
+		log.info("Baz by proxy");
 		var restTemplate = new RestTemplate(); // Yes, should be a bean.
 		return restTemplate.getForObject("http://localhost:8081/baz", BazDto.class);
 	}
